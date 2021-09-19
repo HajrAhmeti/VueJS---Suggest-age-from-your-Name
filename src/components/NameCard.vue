36b9eb62490b217452ff5f716dbedaa3af5c83bc
HajrAhmeti <hajreahmeti@gmail.com>
2021-09-19 15:50:40 +0200

    Suggest your age from your name - vue js

---
+++ src/components/NameCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="name">
+                {{ name }}
+            </div>
+            <div class="age">
+                {{ age }}   
+            </div>
+            <div class="count">
+                {{ count }}
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default {
+    props: ['name','age','count']
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
